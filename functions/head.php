2edb2f6628fab633737b963f0822d5b34b2b4cb6
M.A. Yalcinkaya <Anildinho@users.noreply.github.com>
2023-05-17 14:11:52 +0200

    V1.0.0

---
+++ functions/head.php
@@ -0,0 +1,152 @@
+<?php function head(){
+
+    echo"<html>
+<head>
+
+    <meta name=\"viewport\" content=\"width=device-width\">
+
+    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\" crossorigin=\"anonymous\">
+    <link rel=\"stylesheet\" href=\"css/index.css\">
+
+    <script src=\"https://code.jquery.com/jquery-3.4.1.slim.min.js\" integrity=\"sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n\" crossorigin=\"anonymous\"></script>
+    <script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js\" integrity=\"sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo\" crossorigin=\"anonymous\"></script>
+    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\" integrity=\"sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6\" crossorigin=\"anonymous\"></script>
+
+</head>
+
+<header>
+    <nav class=\"navbar navbar-expand-lg navbar-light bg-light\">
+        <div id=\"navbarNav\">
+            <ul class=\"navbar-nav\">
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText \" href=\"#\">Alle modellen</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">A1</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">A2</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">A3</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">A4</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">A5</a>
+                </li >
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">A6</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">A7</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">A8</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">Q2</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">Q3</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">Q5</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">Q7</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\" >
+                    <a class=\"nav-link navbarText\" href=\"#\">Q8</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">TT</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">R8</a >
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">RS</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">Elektrisch</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"#\">Plug-in Hybride</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>";
+};
+?>
+
+<?php function head2(){
+
+    echo"<html>
+<head>
+
+    <meta name=\"viewport\" content=\"width=device-width\">
+
+    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\" integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\" crossorigin=\"anonymous\">
+    <link rel=\"stylesheet\" href=\"../css/index.css\">
+
+    <script src=\"https://code.jquery.com/jquery-3.4.1.slim.min.js\" integrity=\"sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n\" crossorigin=\"anonymous\"></script>
+    <script src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js\" integrity=\"sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo\" crossorigin=\"anonymous\"></script>
+    <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\" integrity=\"sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6\" crossorigin=\"anonymous\"></script>
+
+</head>
+
+<header>
+    <nav class=\"navbar navbar-expand-lg navbar-light bg-light\">
+        <div id=\"navbarNav\">
+            <ul class=\"navbar-nav\">
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText \" href=\"../index.php\">Alle modellen</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"1-serie.php\">1</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"2-serie.php\">2</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"3-serie.php\">3</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"4-serie.php\">4</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"5-serie.php\">5</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"6-serie.php\">6</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"7-serie.php\">7</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"8-serie.php\">8</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"x-serie.php\">X</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"z-serie.php\">Z4</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"i-serie.php\">i</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"phev-serie.php\">Plug-in Hyrbride</a>
+                </li>
+                <li class=\"nav-item active paddingRightNav\">
+                    <a class=\"nav-link navbarText\" href=\"m-serie.php\">M</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>";
+};
+?>
